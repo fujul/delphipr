654fa1f7811b324a62f0ed1c40ff97fc16d670bc
Julia <fjmail@ukr.net>
2026-04-18 23:08:27 +0300

    Initial commit

---
+++ Unit1.pas
@@ -0,0 +1,29 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Data.Win.ADODB, Vcl.Grids,
+  Vcl.DBGrids;
+
+type
+  TForm1 = class(TForm)
+    ADOConnection1: TADOConnection;
+    DataSource1: TDataSource;
+    ADOTable1: TADOTable;
+    DBGrid1: TDBGrid;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
